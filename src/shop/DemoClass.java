995977590ac5af4643a2a5dbd09ef66075af915c
Pavlo <pavlik99tishkov@gmail.com>
2020-04-02 18:50:00 +0300

    Refactoring

--- src/shop/DemoClass.java
+++ src/shop/DemoClass.java
@@ -1,4 +1,6 @@
-package shop.model;
+package shop;
+
+import shop.model.*;
 
 import java.util.ArrayList;
 import java.util.Comparator;
--- src/shop/Variant.java
+++ src/shop/Variant.java
@@ -1,4 +1,4 @@
-package shop.model;
+package shop;
 
 public enum Variant {
     CATALOGS, CATALOG, TO_BASKET, BUY_PRODUCTS, BEGIN
